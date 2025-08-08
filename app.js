f1fba30022b107bbef74ac75801ea2f5c79e1052
Regnier251 <regnier251hd@gmail.com>
2025-08-08 10:18:55 -0600

    Chat conversacional de una pequeña tienda de abarrotes

---
+++ app.js
@@ -0,0 +1,74 @@
+// Importar dependencias
+import express from "express";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+//Cargar configuracion (api key)
+dotenv.config();
+
+//Cargar express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Servir pagina estatica(frontend)
+app.use("/",express.static("public"));
+
+//Crear middleware para procesar JSON
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+//Instancia de openai y pasar el apikey
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+//Ruta / endpoint / url
+app.post("/api/chatbot", async(req,res)=> {
+    
+    const context = `
+        Eres un asistente de soporte para el supermercado "Aborrotes Don Ramón".
+        Información del negocio:
+            -Ubicación: Lomas de San Francisco 8, Lomas de Cervera, 36257 Marfil, Gto.
+            -Horario: Lunes a Viernes de 9:00am a 5:00pm.
+            -Productos: Pan, Leche, Huevos, Frutas, Carnes y bebidas
+            -Metodos de pago: Efectivo, tarjeta
+        Solo puedes responder preguntas sobre la tienda. Cualquier otra pregunta está prohibida.
+    `;
+
+    //Recibir pregunta del usuario
+    const {message} = req.body;
+
+    if(!message) return res.status(404).json({error: "Has mandado un mensaje vacío!!"});
+
+    //Petición al modelo de intelígencia artificial
+    try{
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-4.1-mini",
+            messages: [
+                {role: "system", content: context},
+                {role: "system", content: "Debes responder de la forma corta y directa, usando los menores tokens posibles."},
+                {role: "user", content: message}
+            ],
+            max_tokens:200
+        })
+
+        //Devolver respuesta
+        const reply = response.choices[0].message.content;
+
+        return res.status(200).json({reply});
+
+    } catch(error) {
+        console.log("Error:", error)
+
+        return res.statur(500).json({
+            error: "Error al general la respuesta"
+        })
+    }
+});
+    
+//Servir el backend
+app.listen(PORT), () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+}
+
---
+++ public/assets/js/main.js
@@ -0,0 +1,44 @@
+const sendButton = document.querySelector("#sendButton");
+const inputText = document.querySelector("#inputText");
+const messagesContainer = document.querySelector(".chat__messages")
+
+const sendMessage = async() =>{
+
+    //Valor del input
+    const myMessage = inputText.value.trim();
+
+    if(!myMessage) return false;
+
+    //Meter mensaje del usuario en la caja de mensajes
+    messagesContainer.innerHTML += `<div class="chat__message chat__message--user">Yo: ${myMessage}</div>`;
+    
+    //Vaciar el input del usuario
+    inputText.value = "";
+
+    //Petición al backend para que me responda la IA
+    try {
+        const response = await fetch('/api/chatbot', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({message:myMessage})
+        })
+    // Incrustar mensaje dle bot en el chat
+    const data = await response.json();
+
+    messagesContainer.innerHTML += `<div class="chat__message chat__message--bot">Don Ramón:${data.reply}</div>`;
+
+    } catch (error) {
+        console.log("error:", error);
+    }
+
+    //Mover el scroll hacia abajo
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+sendButton.addEventListener("click", sendMessage);
+inputText.addEventListener("keypress", (event) => {
+    if(event.key === "Enter") {
+        event.preventDefault();
+        sendMessage();
+    }
+})
